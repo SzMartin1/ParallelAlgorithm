b3d4ed08c0857a6e635283e09b0f952e7f7dc73c
SzMartin1 <78854567+SzMartin1@users.noreply.github.com>
2022-05-07 18:50:16 +0200

    trapezoidal method

---
+++ Beadando_OPENMP/trapezoidal_sequential.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define A -15 /*kezdeti korlát*/
+#define B 15 /* végső korlát*/
+#define EPS 0.000001 /*pontosság*/
+
+
+ double f(double x);
+ double trapezoidal(double f(double x), double a, double b, int n);
+
+
+/*Elkezdődik a program*/
+int main(){
+  int i=2;
+  double integral,integral_new;
+  time_t start, stop;
+  double runtime;
+
+  integral_new=trapezoidal(f,A,B,i);
+
+  /* Perform integration by trapezoidal rule for different number of sub-intervals until they converge to the given accuracy:*/
+  start=clock();
+    do{
+	printf("\n %d",i);
+      integral=integral_new;
+      i++;
+      integral_new=trapezoidal(f,A,B,i);
+     }while(fabs(integral_new-integral)>=EPS);
+  stop=clock();
+
+  runtime = (double)(stop-start)/CLOCKS_PER_SEC;
+  /*Print the answer */
+  printf("\nThe integral is: %lf\n with %d intervals",integral_new,i);
+  printf("\n Sequential runtime is %f sec.",runtime);
+
+  return 0;
+}
+
+/*Itt definiáljuk a függvényt, amelyet használni fogunk */
+double f(double x){
+  return x*x;
+}
+
+/* A trapézmódszer függvénydefiníciója */
+double trapezoidal(double f(double x), double a, double b, int n)
+{
+  double x,h,sum=0,integral;
+  int i;
+  h=fabs(b-a)/n;
+  for(i=1;i<n;i++){
+    x=a+i*h;
+    sum=sum+f(x);
+  }
+  integral=(h/2)*(f(a)+f(b)+2*sum);
+  return integral;
+}
